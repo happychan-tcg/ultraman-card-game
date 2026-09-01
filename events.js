6868c68b874ccb9cd27176785378e2d5fcfa0f57
happychan-tcg <keichi.0223@gmail.com>
2026-09-01 15:50:49 +0900

    Add files via upload

---
+++ events.js
@@ -0,0 +1,16 @@
+// ウルトラマンカードゲーム 大会情報
+// このファイルだけ編集すれば、大会の追加・変更ができます。
+
+const events = [
+{date:"2026-09-02",time:"19:30",title:"2デッキBO3限定マッチ",store:"港北いわや",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-04",time:"18:30",title:"チャレンジマッチ",store:"TCバトロコ秋田駅前",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-09",time:"19:30",title:"2デッキBO3限定マッチ",store:"港北いわや",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-11",time:"18:30",title:"チャレンジマッチ",store:"TCバトロコ秋田駅前",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-16",time:"19:30",title:"2デッキBO3限定マッチ",store:"港北いわや",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-18",time:"18:30",title:"チャレンジマッチ",store:"TCバトロコ秋田駅前",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-23",time:"19:30",title:"2デッキBO3限定マッチ",store:"港北いわや",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-23",time:"20:30",title:"チャレンジマッチ",store:"カードショップアビス",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-25",time:"18:30",title:"チャレンジマッチ",store:"TCバトロコ秋田駅前",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-28",time:"18:30",title:"チャレンジマッチ",store:"お宝弁財天 feat.パンドラ",fee:"500円",capacity:"16名",note:""},
+{date:"2026-09-30",time:"19:30",title:"2デッキBO3限定マッチ",store:"港北いわや",fee:"500円",capacity:"16名",note:""}
+];
